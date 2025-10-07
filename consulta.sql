62618501dde968a877ee80e41a1612025711983c
Rene Pedraza Zuñiga <rene.zuniga261@gmail.com>
2025-10-07 07:59:58 -0600

    Actualizo consulta

--- consulta.sql
+++ consulta.sql
@@ -1,4 +1,5 @@
 -- Consulta de prueba
-SELECT cento_consimo, ubicacion, lugar, area, encargado, venta
+SELECT cento_consumo, ubicacion, lugar, area, encargado, venta
 FROM empleados
 WHERE venta > 30000;
+ORDER BY venta DESC;
